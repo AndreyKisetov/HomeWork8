8fdb9c7367d74a601e84bb016ec611fd442b7390
Andrey Kisetov <rbcfff@yandex.ru>
2022-07-03 06:54:01 +0300

    added fourth Program.cs

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace HelloWorld
+{
+    // Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N. (не включая N)
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число N: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"{N} -> ");
+            for (int i = 2; i < N+1; i = i +2)
+            {
+                if (i < N)
+                Console.Write($"{i}, ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
